e0fac1f61103b538e6d642468a96371e82e2e8de
mamun-prodhan <mamunbranding4u@gmail.com>
2023-06-04 16:43:08 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            books: 'The song of ice and fire',
+            author: 'Daniyel Difo',
+            age: 25
+        }
+    }
+})
+
+app.mount('#app')
